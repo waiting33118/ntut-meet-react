11080d01e8f105af38739a1e25e4d74fd108f1e8
Tony Chung <waiting33118@gmail.com>
2021-08-27 15:15:43 +0800

    feat: add getUserMedia & usernamr input feature

--- src/components/Home/Content/Left.tsx
+++ src/components/Home/Content/Left.tsx
@@ -1,5 +1,40 @@
+import { ChangeEvent, useState } from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../../../store/UserContext';
+import WebRTC from '../../../libs/WebRTC';
+import { useHistory } from 'react-router-dom';
+
 const Left = () => {
-  const joinRoomHandler = () => {};
+  const history = useHistory();
+  const ctx = useContext(UserContext);
+  const { setUserInfo } = ctx!;
+  const [username, setUsername] = useState('');
+  const [isClick, setIsClick] = useState(false);
+  const webRTC = new WebRTC();
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const joinRoomHandler = async () => {
+    setIsClick(true);
+    if (username.trim() === '') {
+      alert('請輸入名稱才可加入至會議室！');
+      setIsClick(false);
+      return;
+    }
+    try {
+      const localStream = await webRTC.getLocalStream();
+      setUserInfo((prev) => ({ ...prev, username, localStream }));
+      history.push('/room');
+    } catch (error) {
+      setIsClick(false);
+      if (error.name === 'NotAllowedError')
+        alert(`${error.message}! 請給予攝影機與麥克風權限`);
+      if (error.name === 'NotFoundError')
+        alert(`${error.message}! 使用者無鏡頭設備，無法加入會議`);
+    }
+  };
 
   return (
     <div
@@ -11,7 +46,7 @@ const Left = () => {
         <div className={'mb-3'}>運用ＷebRTC技術打造出的</div>
         <div>小型會議室</div>
         <div className={'mt-8 text-xl text-gray-600'}>
-          在疫情時代下，師生、團隊的最佳溝通利器
+          在疫情時代下，師生、團隊的最佳遠距溝通服務
         </div>
       </div>
       <div className={'w-full divide-y-2 divide-gray-200'}>
@@ -24,10 +59,24 @@ const Left = () => {
             className={
               'w-60 mr-4 p-3 border border-gray-400 hover:border-gray-900 focus:border-blue-500 rounded-md'
             }
+            value={username}
+            onChange={handleInputChange}
           />
-          <button onClick={joinRoomHandler}>加入</button>
+          <button onClick={joinRoomHandler} disabled={isClick}>
+            加入
+          </button>
         </div>
-        <div className={'py-5 font-light'}>進一步了解 Ntut Meet</div>
+        <div className={'py-5 font-light'}>
+          <a
+            href="https://github.com/waiting33118/ntut-meet-react"
+            rel="noreferrer"
+            target="_blank"
+            className={'mr-1 text-blue-600 underline'}
+          >
+            進一步了解
+          </a>
+          Ntut Meet
+        </div>
       </div>
     </div>
   );
